d289e322c8f2f91b96b390b55bde436a0936d3b2
Kardzhaliyski <Zdravko.kardzhaliyski@gmail.com>
2022-03-11 13:12:54 +0200

    First Spring Data App

---
+++ SpringData/1.DBAppsIntroduction/src/diablo/Main.java
@@ -0,0 +1,46 @@
+package diablo;
+
+import java.sql.*;
+import java.util.Properties;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter username (default: root): ");
+        String user = sc.nextLine();
+        user = user.equals("") ? "root" : user;
+        System.out.println();
+        System.out.print("Enter password (default: empty): ");
+        String password = sc.nextLine().trim();
+
+        Properties props = new Properties();
+        props.setProperty("user", user);
+        props.setProperty("password", password);
+
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/diablo", props);
+
+//        PreparedStatement preparedStatement = connection.prepareStatement(
+//                "SELECT user_name,u.first_name, u.last_name, COUNT(ug.user_id) AS game_count FROM users AS u JOIN users_games AS ug ON u.id = ug.user_id  GROUP BY u.id");
+
+//        String username = "nakov";
+//        preparedStatement.setString(1, username);
+
+        PreparedStatement preparedStatement = connection.prepareStatement(
+                "SELECT user_name,u.first_name, u.last_name, COUNT(ug.user_id) AS game_count FROM users AS u JOIN users_games AS ug ON u.id = ug.user_id  GROUP BY u.id");
+
+        ResultSet rs = preparedStatement.executeQuery();
+
+        while (rs.next()) {
+            String currentUsername = rs.getString("user_name");
+            String firstName = rs.getString("first_name");
+            String lastName = rs.getString("last_name");
+            String gamesPlayed = rs.getString("game_count");
+            System.out.printf("User: %s%n", currentUsername);
+            System.out.printf("%s %s has played %s games%n", firstName, lastName, gamesPlayed);
+        }
+
+        connection.close();
+
+    }
+}
