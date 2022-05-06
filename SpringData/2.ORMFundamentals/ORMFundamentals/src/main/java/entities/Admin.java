4fec110e2dd94983aeae4f548a2e9f67078c661e
Kardzhaliyski <Zdravko.kardzhaliyski@gmail.com>
2022-05-06 13:01:43 +0300

    Added new entity

---
+++ SpringData/2.ORMFundamentals/ORMFundamentals/src/main/java/entities/Admin.java
@@ -0,0 +1,71 @@
+package entities;
+
+import annotations.Column;
+import annotations.Entity;
+import annotations.Id;
+
+import java.time.LocalDate;
+
+@Entity(name = "admins")
+public class Admin {
+
+    @Id
+    @Column(name = "id")
+    private long id;
+    @Column(name = "username")
+    private String username;
+    @Column(name = "age")
+    private int age;
+    @Column(name = "registration_date")
+    private LocalDate registrationDate;
+    @Column(name = "town")
+    private String town;
+
+    private Admin(){};
+
+    public Admin(String username, int age, LocalDate registration) {
+        this.username = username;
+        this.age = age;
+        this.registrationDate = registration;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    private void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    private void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    private void setAge(int age) {
+        this.age = age;
+    }
+
+    public LocalDate getRegistrationDate() {
+        return registrationDate;
+    }
+
+    private void setRegistrationDate(LocalDate registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+}
