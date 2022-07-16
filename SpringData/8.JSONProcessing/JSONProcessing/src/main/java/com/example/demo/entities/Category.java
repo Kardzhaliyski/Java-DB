957aa0961ee29d25b3bad158d8c96133277a4e1c
Kardzhaliyski <Zdravko.kardzhaliyski@gmail.com>
2022-07-16 10:09:01 +0300

    Implemented Category entity

--- SpringData/8.JSONProcessing/JSONProcessing/src/main/java/com/example/demo/entities/Category.java
+++ SpringData/8.JSONProcessing/JSONProcessing/src/main/java/com/example/demo/entities/Category.java
@@ -1,8 +1,50 @@
 package com.example.demo.entities;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @ManyToMany(mappedBy = "categories")
+    private final Set<Product> products;
 
+    protected Category() {
+        products = new HashSet<>();
+    }
+
+    public Category(String name) {
+        this();
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return Collections.unmodifiableSet(products);
+    }
+
+    public void addProduct(Product product) {
+        this.products.add(product);
+    }
 }
